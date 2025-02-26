def19807a69746ae37c1f43ef010de759c1970cb
Jeno Saghi <jeno.2010@yahoo.com>
2025-02-26 15:57:21 -0700

    made the train function generic so it will work with any model, train loader, and val loader

--- source/train.py
+++ source/train.py
@@ -6,15 +6,13 @@ import json
 
 
 # Function to train and evaluate expander models 
-def expander_loop(model, ratio, num_epochs, save_name):
+def train(model, train_loader, val_loader, num_epochs, save_name):
 
   device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
   model.to(device)
 
   optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
   loss_fn = nn.MSELoss()
-
-  train, val, _ = data.get_temp(ratio)
 
   best_val_loss = float('inf')
   history = {'train_loss': [], 'val_loss': []}
@@ -23,7 +21,7 @@ def expander_loop(model, ratio, num_epochs, save_name):
     model.train(True)
     running_loss = 0.0
 
-    for i, (inputs, targets) in enumerate(train):
+    for i, inputs, targets in enumerate(train_loader):
       inputs, targets = inputs.to(device), targets.to(device)
       optimizer.zero_grad()
       outputs = model(inputs)
@@ -36,29 +34,29 @@ def expander_loop(model, ratio, num_epochs, save_name):
       if i % 1000 == 999:
         print(f'Epoch {epoch + 1}, Step [{i + 1}/{len(train)}], Loss: {loss.item():.4f}')
 
-    running_loss /= len(train)
+    running_loss /= len(train_loader)
     print(f'Epoch {epoch + 1}, Loss: {running_loss}')
 
     model.train(False)
     val_loss = 0.0
     with torch.no_grad():
-      for inputs, targets in val:
+      for inputs, targets in val_loader:
         inputs, targets = inputs.to(device), targets.to(device)
         outputs = model(inputs)
         loss = loss_fn(outputs, targets)
         val_loss += loss.item()
 
-    val_loss /= len(val)
+    val_loss /= len(val_loader)
     print(f'Val Loss: {val_loss}')
 
     history['train_loss'].append(running_loss)
     history['val_loss'].append(val_loss)
 
-    history_save_path = f'{HISTORY_PATH + save_name}_{epoch}'
+    history_save_path = f'{HISTORY_PATH + save_name}_{epoch}.json'
     with open(history_save_path, 'w') as f:
       json.dump(history, f)
 
-    model_save_path = f'{MODEL_PATH + save_name}_{epoch}'
+    model_save_path = f'{MODEL_PATH + save_name}_{epoch}.pth'
     if val_loss < best_val_loss:
       best_val_loss = val_loss
       torch.save(model.state_dict(), model_save_path)
