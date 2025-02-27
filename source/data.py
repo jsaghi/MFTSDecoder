03f76ccd1893e934bab7a737365716fd601cf162
Jeno Saghi <jeno.2010@yahoo.com>
2025-02-27 15:00:21 -0700

    changed the way data are normalized and built a function to handle normalization

--- source/data.py
+++ source/data.py
@@ -2,6 +2,8 @@ from settings import *
 import torch
 from torch.utils.data import Dataset, DataLoader
 import pandas as pd
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
 
 
 # Downsampling dataset class for training and evaluating decoder models
@@ -10,8 +12,6 @@ class LFData(Dataset):
     self.data = data
     self.seq_length = seq_length
     self.downsample_ratio = downsample_ratio
-    self.mean = data.mean(axis=0)
-    self.std = data.std(axis=0)
 
   def __len__(self):
     return len(self.data) - self.seq_length
@@ -20,28 +20,22 @@ class LFData(Dataset):
     tensor = torch.tensor(self.data[index:index + self.seq_length], dtype=torch.float32)
     tensor = tensor.unsqueeze(0)
     downsample = tensor[::, self.downsample_ratio - 1::self.downsample_ratio]
-    downsample -= self.mean
-    downsample /= self.std
     return downsample, tensor
     
 
 # Time series dataset class for evaluating predction models without decoders attached
 class TSData(Dataset):
   def __init__(self, data, seq_length, delay):
-    self.data = data
-    self.targets = data[:, 1]
+    self.data = data[:, :-1]
+    self.targets = data[:, -1]
     self.seq_length = seq_length
     self.delay = delay
-    self.mean = data.mean(axis=0)
-    self.std = data.std(axis=0)
 
   def __len__(self):
     return len(self.data) - (self.seq_length + self.delay)
   
   def __getitem__(self, index):
     tensor = torch.tensor(self.data[index:index + self.seq_length], dtype=torch.float32)
-    tensor -= self.mean
-    tensor /= self.std
     target = torch.tensor(self.targets[index + self.seq_length + self.delay], dtype=torch.float32)
     return tensor, target
 
@@ -50,18 +44,9 @@ class TSData(Dataset):
 class MFTSData(Dataset):
   def __init__(self, lf_data, if_data, hf_data, ds_ratio1, ds_ratio2, seq_length, delay):
     self.lf_data = lf_data
-    self.lf_mean = lf_data.mean(axis=0)
-    self.lf_std = lf_data.std(axis=0)
-
     self.if_data = if_data
-    self.if_mean = if_data.mean(axis=0)
-    self.if_std = if_data.std(axis=0)
-
-    self.hf_data = hf_data
-    self.hf_mean = hf_data.mean(axis=0)
-    self.hf_std = hf_data.std(axis=0)
-
-    self.targets = lf_data[:, 0]
+    self.hf_data = hf_data[:, :-1]
+    self.targets = hf_data[:, -1]
 
     self.ds_ratio1 = ds_ratio1
     self.ds_ratio2 = ds_ratio2
@@ -73,17 +58,8 @@ class MFTSData(Dataset):
   
   def __getitem__(self, index):
     lf_tensor = torch.tensor(self.lf_data[index:index + self.seq_length], dtype=torch.float32)
-    lf_tensor -= self.lf_mean
-    lf_tensor /= self.lf_std
-
     if_tensor = torch.tensor(self.if_data[index:index + self.seq_length], dtype=torch.float32)
-    if_tensor -= self.if_mean
-    if_tensor /= self.if_std
-
     hf_tensor = torch.tensor(self.hf_data[index:index + self.seq_length], dtype=torch.float32)
-    hf_tensor -= self.hf_mean
-    hf_tensor /= self.hf_std
-
     target = torch.tensor(self.targets[index + self.seq_length + self.delay], dtype=torch.float32)
 
     lf_tensor = lf_tensor[self.ds_ratio1 - 1::self.ds_ratio1, :]
@@ -104,6 +80,37 @@ def train_val_test_split(data):
   val = data[-validate_split:-test_split]
   test = data[-test_split:]
   return train, val, test
+
+
+# Function to return normalized values of the jena climate dataset. Right-skewed features
+# Have a modified logarithmic transform applied, and then min-max scaling is applied to all
+# features except for temperature. This is because temperature will be the target, so 
+# min-max scaling is applied to temperature inside of the relevant torch Dataset classes
+def jena_normalize():
+  jena = pd.read_csv(JENA_PATH)
+  jena.drop(['Tpot (K)'], axis=1, inplace=True)
+  date_time = jena['Date Time']
+  targets = jena['T (degC)'].rename('Targets')
+  log_transform = jena[['VPmax (mbar)',
+                        'VPdef (mbar)',
+                        'sh (g/kg)',
+                        'H2OC (mmol/mol)',
+                        'wv (m/s)',
+                        'max. wv (m/s)']].apply(lambda x: np.log(x + 1))
+  jena.drop(['Date Time',
+             'VPmax (mbar)',
+             'VPdef (mbar)',
+             'sh (g/kg)',
+             'H2OC (mmol/mol)',
+             'wv (m/s)',
+             'max. wv (m/s)'], axis=1, inplace=True)
+  
+  scaler = MinMaxScaler()
+  logt_scaled = pd.DataFrame(scaler.fit_transform(log_transform),
+                             columns=log_transform.columns)
+  jena_scaled = pd.DataFrame(scaler.fit_transform(jena), columns=jena.columns)
+  output_df = pd.concat([date_time, jena_scaled, logt_scaled, targets], axis=1)
+  return output_df
 
 
 # Function that returns a dataloader for train and validation datasets of just the 
@@ -127,9 +134,9 @@ def get_temp(downsample_ratio):
 # a dataloader for train, validation, and test datasets. Used to train prediction models without
 # attached decoders
 def get_ts():
-  jena = pd.read_csv(JENA_PATH)
-  jena.drop(['Tpot (K)', 'Date Time'], axis=1, inplace=True)
-  train, val, test = train_val_test_split(jena.to_numpy())
+  data = jena_normalize()
+  data.drop(['Date Time'], axis=1, inplace=True)
+  train, val, test = train_val_test_split(data.to_numpy())
 
   train_ds = TSData(train, SEQ_LENGTH, DELAY)
   val_ds = TSData(val, SEQ_LENGTH, DELAY)
@@ -145,11 +152,11 @@ def get_ts():
 # Function that returns train, validation, and test dataloaders for mixed frequency multivariate
 # time series. This is the data pipline to train and test the combined prediction models
 def get_mfts():
-  jena = pd.read_csv(JENA_PATH)
-  jena.drop(['Tpot (K)', 'Date Time'], axis=1, inplace=True)
-  lf_data = jena[['T (degC)', 'Tdew (degC)', 'rh (%)', 'sh (g/kg)', 'H2OC (mmol/mol)']].to_numpy()
-  if_data = jena[['p (mbar)', 'VPmax (mbar)', 'VPact (mbar)', 'VPdef (mbar)']].to_numpy()
-  hf_data = jena[['rho (g/m**3)', 'wv (m/s)', 'max. wv (m/s)', 'wd (deg)']].to_numpy()
+  data = jena_normalize()
+  data.drop(['Date Time'], axis=1, inplace=True)
+  lf_data = data[['T (degC)', 'Tdew (degC)', 'rh (%)', 'sh (g/kg)', 'H2OC (mmol/mol)']].to_numpy()
+  if_data = data[['p (mbar)', 'VPmax (mbar)', 'VPact (mbar)', 'VPdef (mbar)']].to_numpy()
+  hf_data = data[['rho (g/m**3)', 'wv (m/s)', 'max. wv (m/s)', 'wd (deg)', 'Targets']].to_numpy()
 
   lf_train, lf_val, lf_test = train_val_test_split(lf_data)
   if_train, if_val, if_test = train_val_test_split(if_data)
