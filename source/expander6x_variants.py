723470899a962d9f7d0d124ab891190d17de02f8
Jeno <jeno.2010@yahoo.com>
2025-03-11 15:59:52 -0600

    Made changes to the 6X expander variants

--- source/expander6x_variants.py
+++ source/expander6x_variants.py
@@ -1,48 +1,54 @@
-from custom_layers import DenseFilterExpansion, FilterAttention
+from custom_layers import DenseFilterExpansion, FilterAttention, ConvInsert
 import torch
 import torch.nn as nn
 import torcn.nn.functional as F
 
 
 # Decoder class to expand by a ratio of 6 to 1
-class Expander6X(nn.Module):
+class Expander6XBase(nn.Module):
   def __init__(self, input_shape):
     super().__init__()
-    self.dfe = DenseFilterExpansion(4096, input_shape[-1])
-    self.conv1 = nn.ConvTranspose1d(in_channels=4096, out_channels=2048,
-                       kernel_size=11, stride=1, padding=0)
-    self.conv2 = nn.ConvTranspose1d(in_channels=2048, out_channels=1024,
-                       kernel_size=11, stride=1, padding=0)
-    self.conv3 = nn.ConvTranspose1d(in_channels=1024, out_channels=512,
-                       kernel_size=11, stride=1, padding=0)
-    self.conv4 = nn.ConvTranspose1d(in_channels=512, out_channels=256,
-                       kernel_size=11, stride=1, padding=0)
-    self.conv5 = nn.ConvTranspose1d(in_channels=256, out_channels=128,
-                       kernel_size=11, stride=1, padding=0)
-    self.conv6 = nn.ConvTranspose1d(in_channels=128, out_channels=64,
-                       kernel_size=11, stride=1, padding=0)
-    self.conv7 = nn.ConvTranspose1d(in_channels=64, out_channels=32,
-                       kernel_size=11, stride=1, padding=0)
-    self.conv8 = nn.ConvTranspose1d(in_channels=32, out_channels=16,
-                       kernel_size=11, stride=1, padding=0)
-    self.conv9 = nn.ConvTranspose1d(in_channels=16, out_channels=8,
-                       kernel_size=5, stride=1, padding=0)
-    self.conv10 = nn.ConvTranspose1d(in_channels=8, out_channels=4,
-                       kernel_size=5, stride=2, padding=2, output_padding=1)
-    self.conv11 = nn.ConvTranspose1d(in_channels=4, out_channels=1,
-                       kernel_size=5, stride=2, padding=2, output_padding=1)
+    self.conv1 = nn.ConvTranspose1d(in_channels=1, out_channels=64, kernel_size=5,
+                      stride=2, padding=2, output_padding=1)
+    self.fa1 = FilterAttention(64, input_shape[-1] * 2)
+    self.ci1 = ConvInsert(336, 8)
+    self.conv2 = nn.ConvTranspose1d(in_channels=1, out_channels=64,
+                       kernel_size=3, stride=1, padding=0)
+    self.conv3 = nn.ConvTranspose1d(in_channels=64, out_channels=128, kernel_size=5,
+                      stride=2, padding=2, output_padding=1)
+    self.fa2 = FilterAttention(128, 1008)
 
   def forward(self, inputs):
-    outputs = F.tanh(self.dfe(inputs))
-    outputs = self.conv1(outputs)
+    outputs = self.conv1(inputs)
+    outputs = self.fa1(outputs)
+    outputs = self.ci1(outputs)
     outputs = self.conv2(outputs)
     outputs = self.conv3(outputs)
-    outputs = self.conv4(outputs)
-    outputs = self.conv5(outputs)
-    outputs = self.conv6(outputs)
-    outputs = self.conv7(outputs)
-    outputs = self.conv8(outputs)
-    outputs = self.conv9(outputs)
-    outputs = self.conv10(outputs)
-    outputs = F.sigmoid(self.conv11(outputs))
+    outputs = self.fa2(outputs)
+    return outputs
+  
+
+# Model which adds the dense filter expansion layer before the first convolutonal layer
+class Expander6XDFE(nn.Module):
+  def __init__(self, input_shape):
+    super().__init__()
+    self.dfe1 = DenseFilterExpansion(num_filters=32, seq_length=input_shape[-1])
+    self.conv1 = nn.ConvTranspose1d(in_channels=32, out_channels=64, kernel_size=5,
+                      stride=2, padding=2, output_padding=1)
+    self.fa1 = FilterAttention(64, input_shape[-1] * 2)
+    self.ci1 = ConvInsert(336, 8)
+    self.conv2 = nn.ConvTranspose1d(in_channels=1, out_channels=64,
+                       kernel_size=3, stride=1, padding=0)
+    self.conv3 = nn.ConvTranspose1d(in_channels=64, out_channels=128, kernel_size=5,
+                      stride=2, padding=2, output_padding=1)
+    self.fa2 = FilterAttention(128, 1008)
+
+  def forward(self, inputs):
+    outputs = self.dfe1(inputs)
+    outputs = self.conv1(outputs)
+    outputs = self.fa1(outputs)
+    outputs = self.ci1(outputs)
+    outputs = self.conv2(outputs)
+    outputs = self.conv3(outputs)
+    outputs = self.fa2(outputs)
     return outputs
