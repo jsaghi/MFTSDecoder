e63c8ad9dccd59f01ed67dcdb9121979176a40b9
Jeno <jeno.2010@yahoo.com>
2025-03-12 00:52:11 -0600

    changed the way the TensorBoardLogger is accessed

--- source/train_decoders.py
+++ source/train_decoders.py
@@ -5,7 +5,7 @@ from expander6x_variants import *
 from expander36x_variants import *
 import lightning as L
 from lightning.pytorch.callbacks import ModelCheckpoint
-from lightning.pytorch.loggers import TensorBoardLogger
+from lightning.pytorch import loggers as pl_loggers
 
 
 # Build two dictionaries of models:
@@ -36,7 +36,7 @@ if_train_loader, if_val_loader, _ = data.get_temp(SEQ_LENGTH // IF_LENGTH, False
 
 # Train all models in the 6x dictionary
 for key, value in dict_6x.items():
-  logger = TensorBoardLogger(HISTORY_PATH + key)
+  logger = pl_loggers.TensorBoardLogger(save_dir=HISTORY_PATH + key)
   checkpoint_callback = ModelCheckpoint(
     monitor='val_loss',
     dirpath=MODEL_PATH,
