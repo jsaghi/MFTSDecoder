40a8da6da18cc82a6ffe64597c3edbe14f8687d6
Jeno <jeno.2010@yahoo.com>
2025-03-14 15:05:04 -0600

    added an early stopping callback

--- source/train_decoders.py
+++ source/train_decoders.py
@@ -6,6 +6,7 @@ from expander6x_variants import *
 from expander36x_variants import *
 import lightning as L
 from lightning.pytorch.callbacks import ModelCheckpoint
+from lightning.pytorch.callbacks.early_stopping import EarlyStopping
 from lightning.pytorch.loggers import CSVLogger
 
 
@@ -13,7 +14,6 @@ from lightning.pytorch.loggers import CSVLogger
 torch.set_float32_matmul_precision('medium')
 
 # Build two dictionaries of models:
-'''
 dict_6x = {
   '6xBase': Expander6XBase(),
   '6xHalfP': Expander6XHalfP(),
@@ -21,8 +21,7 @@ dict_6x = {
   '6xDFE': Expander6XDFE(),
   '6xDFEHalfP': Expander6XHalfP(),
   '6xDFEDoubleP': Expander6XDFEDoubleP()
-}'
-'''
+}
 
 dict_36x = {
   #'36xConv': Expander36XConvOnly(),
@@ -40,42 +39,49 @@ dict_36x = {
 lf_train_loader, lf_val_loader, _ = data.get_temp(SEQ_LENGTH // LF_LENGTH, False)
 if_train_loader, if_val_loader, _ = data.get_temp(SEQ_LENGTH // IF_LENGTH, False)
 
-'''
+# Prepare early stopping callback
+early_stopping = EarlyStopping(
+  monitor='val_loss',
+  min_delta=1e-3,
+  mode='min',
+  patience=3,
+  verbose=True
+  )
+
 # Train all models in the 6x dictionary
 for key, value in dict_6x.items():
   logger = CSVLogger(save_dir=HISTORY_PATH + key)
-  checkpoint_callback = ModelCheckpoint(
+  checkpoint = ModelCheckpoint(
     monitor='val_loss',
     dirpath=MODEL_PATH,
-    filename=key + '-e{epoch}',
-    save_top_k=10,
+    filename=key + '-{epoch}',
+    save_top_k=3,
     mode='min',
     verbose=True
   )
   trainer = L.Trainer(
-    max_epochs=10,
+    max_epochs=30,
     logger=logger,
-    callbacks=[checkpoint_callback],
+    callbacks=[checkpoint, early_stopping],
   )
 
   trainer.fit(LightningDecoder(value), if_train_loader, if_val_loader)
-'''
 
 # Train all models in the 36x dictionary
 for key, value in dict_36x.items():
   logger = CSVLogger(HISTORY_PATH + key)
-  checkpoint_callback = ModelCheckpoint(
+  checkpoint = ModelCheckpoint(
     monitor='val_loss',
     dirpath=MODEL_PATH,
-    filename=key + '-e{epoch}',
-    save_top_k=10,
+    filename=key + '-{epoch}',
+    save_top_k=3,
     mode='min',
     verbose=True
   )
   trainer = L.Trainer(
-    max_epochs=10,
+    max_epochs=30,
     logger=logger,
-    callbacks=[checkpoint_callback],
+    callbacks=[checkpoint, early_stopping],
   )
 
   trainer.fit(LightningDecoder(value), lf_train_loader, lf_val_loader)
