09c0c3f1302cfce54d5c1010e16be2cd66a80bb8
Jeno <jeno.2010@yahoo.com>
2025-03-17 19:22:08 -0600

    changed name to tft_2 for differentiation

--- source/train_tft.py
+++ source/train_tft.py
@@ -17,11 +17,11 @@ early_stopping = EarlyStopping(
   patience=5,
   verbose=True
   )
-logger = CSVLogger(save_dir=HISTORY_PATH + 'tft_1')
+logger = CSVLogger(save_dir=HISTORY_PATH + 'tft_2')
 checkpoint = ModelCheckpoint(
     monitor='val_loss',
     dirpath=MODEL_PATH,
-    filename='tft1' + '-{epoch}',
+    filename='tft_2' + '-{epoch}',
     save_top_k=5,
     mode='min',
     verbose=True
