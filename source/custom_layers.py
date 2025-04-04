8c771c69ce65d1920379dcafa5e1a3cbb5c2b477
Jeno <jeno.2010@yahoo.com>
2025-04-04 08:54:17 -0600

    added comments

--- source/custom_layers.py
+++ source/custom_layers.py
@@ -65,25 +65,44 @@ class FilterAttention(nn.Module):
 class ConvInsert(nn.Module):
   def __init__(self, input_size, kernel_size):
     super().__init__()
+    # Save input kernel size and use it to calculate the output size
     self.kernel_size = kernel_size
     self.output_length = (4 * input_size) // kernel_size - 2
+
+    # Initialize weights and biases for the two kernels
     self.w1 = nn.Parameter(torch.randn(self.kernel_size, 1))
     self.w2 = nn.Parameter(torch.randn(self.kernel_size, 1))
     self.b = nn.Parameter(torch.zeros(self.output_length, 1))
 
+  # Forward method of the custom module
   def forward(self, inputs):
+    # Split the input into equal chunks of half the length of the kernel size
     in_slices = torch.split(inputs, self.kernel_size // 2, -1)
+    # Create a list to store new output slices
     out_slices = []
+
+    # Iterate through each slice of the input sequence
     for i in range(len(in_slices) - 1):
+      # Combine adjacent input slices so they can be operated on by the kernels
       super_slice = torch.cat((in_slices[i], in_slices[i + 1]), dim=-1)
+
+      # Calculate outputs whcih while spliced into the original sequence
       out1 = torch.matmul(super_slice, self.w1)
       out2 = torch.matmul(super_slice, self.w2)
       out1 += self.b[2 * i]
       out2 += self.b[2 * i + 1]
+
+      # Concatenate the outputs
       out = torch.cat((out1, out2), dim=-1)
+
+      # Concatenate the one of the input slices with the outputs
       out_slices.append(torch.cat((in_slices[i], out), dim=-1))
+      # When the end of the input sequence is reached, concatenate the last input
+      # slice without adding an output
       if i == len(in_slices) - 2:
         out_slices.append(in_slices[i + 1])
+    
+    # Concatenate and return the output slices in a single tensor
     outputs = torch.cat(out_slices, dim=-1)
     return outputs
   
