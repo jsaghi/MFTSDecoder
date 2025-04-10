698e69f7d8788869ac55e18ec30260da866804cd
Jeno <jeno.2010@yahoo.com>
2025-04-10 09:12:10 -0600

    training new hf tft

--- source/train_tft.py
+++ source/train_tft.py
@@ -10,7 +10,7 @@ from lightning.pytorch.loggers import CSVLogger
 # Set torch.matmul precision
 torch.set_float32_matmul_precision('high')
 
-training, train, val, _ = data.get_time_series(downsample_ratio=36)
+training, train, val, _ = data.get_time_series()
 base_tft = tft.build_tft(training)
 lightning_tft = tft.LightningTFT(base_tft)
 
@@ -21,11 +21,11 @@ early_stopping = EarlyStopping(
   patience=5,
   verbose=True
   )
-logger = CSVLogger(save_dir=HISTORY_PATH + 'lf_tft')
+logger = CSVLogger(save_dir=HISTORY_PATH + 'hf_tft_4')
 checkpoint = ModelCheckpoint(
     monitor='val_loss',
     dirpath=MODEL_PATH,
-    filename='lf_tft' + '-{epoch}',
+    filename='hf_tft_4' + '-{epoch}',
     save_top_k=5,
     mode='min',
     verbose=True
