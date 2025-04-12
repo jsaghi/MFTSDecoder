b4d8613072fd8af54d3db3cf65de574653f49c66
Jeno Saghi <jeno.2010@yahoo.com>
2025-04-12 10:39:31 -0600

    updated upsampling architectures to reflect best performing architectures after testing

--- source/expanders.py
+++ source/expanders.py
@@ -8,55 +8,48 @@ import lightning as L
 
 # Class to expand an input by a ratio of 36 to 1
 class Expander36X(nn.Module):
-  def __init__(self, input_shape):
+  def __init__(self):
     super().__init__()
-    self.dfe = DenseFilterExpansion(4096, input_shape[-1])
-    self.conv1 = nn.ConvTranspose1d(in_channels=4096, out_channels=2048,
-                       kernel_size=3, stride=1, padding=0)
-    self.conv2 = nn.ConvTranspose1d(in_channels=2048, out_channels=1024,
+    self.conv1 = nn.ConvTranspose1d(in_channels=1, out_channels=1024,
                        kernel_size=5, stride=2, padding=2, output_padding=1)
-    self.conv3 = nn.ConvTranspose1d(in_channels=1024, out_channels=512,
+    self.conv2 = nn.ConvTranspose1d(in_channels=1024, out_channels=512,
                        kernel_size=5, stride=2, padding=2, output_padding=1)
-    self.conv4 = nn.ConvTranspose1d(in_channels=512, out_channels=256,
-                       kernel_size=3, stride=1, padding=0)
-    self.conv5 = nn.ConvTranspose1d(in_channels=256, out_channels=128,
-                       kernel_size=3, stride=1, padding=0)
-    self.conv6 = nn.ConvTranspose1d(in_channels=128, out_channels=64,
-                       kernel_size=3, stride=1, padding=0)
-    self.conv7 = nn.ConvTranspose1d(in_channels=64, out_channels=32,
+    self.fa1 = FilterAttention(512, 112)
+    self.ci1 = ConvInsert(112, 28)
+    self.conv3 = nn.ConvTranspose1d(in_channels=1, out_channels=2048,
                        kernel_size=5, stride=2, padding=2, output_padding=1)
-    self.conv8 = nn.ConvTranspose1d(in_channels=32, out_channels=16,
+    self.conv4 = nn.ConvTranspose1d(in_channels=2048, out_channels=1024,
                        kernel_size=5, stride=2, padding=2, output_padding=1)
-    self.conv9 = nn.ConvTranspose1d(in_channels=16, out_channels=1,
+    self.conv5 = nn.ConvTranspose1d(in_channels=1024, out_channels=512,
                        kernel_size=5, stride=2, padding=2, output_padding=1)
+    self.fa2 = FilterAttention(512, SEQ_LENGTH)
 
   def forward(self, inputs):
-    outputs = F.tanh(self.dfe(inputs))
-    outputs = self.conv1(outputs)
+    outputs = self.conv1(inputs)
     outputs = self.conv2(outputs)
+    outputs = self.fa1(outputs)
+    outputs = self.ci1(outputs)
     outputs = self.conv3(outputs)
     outputs = self.conv4(outputs)
     outputs = self.conv5(outputs)
-    outputs = self.conv6(outputs)
-    outputs = self.conv7(outputs)
-    outputs = self.conv8(outputs)
-    outputs = F.sigmoid(self.conv9(outputs))
+    outputs = self.fa2(outputs)
+    outputs = F.sigmoid(outputs)
     return outputs
   
 
 # Decoder class to expand by a ratio of 6 to 1
 class Expander6X(nn.Module):
-  def __init__(self, input_shape):
+  def __init__(self):
     super().__init__()
     self.conv1 = nn.ConvTranspose1d(in_channels=1, out_channels=64, kernel_size=5,
                       stride=2, padding=2, output_padding=1)
-    self.fa1 = FilterAttention(64, input_shape[-1] * 2)
+    self.fa1 = FilterAttention(64, 336)
     self.ci1 = ConvInsert(336, 8)
     self.conv2 = nn.ConvTranspose1d(in_channels=1, out_channels=64,
                        kernel_size=3, stride=1, padding=0)
     self.conv3 = nn.ConvTranspose1d(in_channels=64, out_channels=128, kernel_size=5,
                       stride=2, padding=2, output_padding=1)
-    self.fa2 = FilterAttention(128, 1008)
+    self.fa2 = FilterAttention(128, SEQ_LENGTH)
 
   def forward(self, inputs):
     outputs = self.conv1(inputs)
@@ -64,7 +57,8 @@ class Expander6X(nn.Module):
     outputs = self.ci1(outputs)
     outputs = self.conv2(outputs)
     outputs = self.conv3(outputs)
-    outputs = F.sigmoid(self.fa2(outputs))
+    outputs = self.fa2(outputs)
+    outputs = F.sigmoid(outputs)
     return outputs
   
 
@@ -73,7 +67,7 @@ class Expander6X(nn.Module):
 class LFExpanderStack(nn.Module):
   def __init__(self, num_features):
     super().__init__()
-    self.expander_stack = [Expander36X((1, LF_LENGTH))] * num_features
+    self.expander_stack = [Expander36X()] * num_features
 
   def forward(self, inputs):
     in_slices = torch.split(inputs, 1, dim=-1)
@@ -88,7 +82,7 @@ class LFExpanderStack(nn.Module):
 class IFExpanderStack(nn.Module):
   def __init__(self, num_features):
     super().__init__()
-    self.expander_stack = [Expander6X((1, IF_LENGTH))] * num_features
+    self.expander_stack = [Expander6X()] * num_features
 
   def forward(self, inputs):
     in_slices = torch.split(inputs, 1, dim=-1)
