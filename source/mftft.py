0ed84b58bde4d2279650527dd21ebf3111f84e7d
Jeno <jeno.2010@yahoo.com>
2025-04-15 19:48:38 -0600

    fixed bad import statement

--- source/mftft.py
+++ source/mftft.py
@@ -1,5 +1,5 @@
 from settings import *
-from source.conv_upsamplers import LFExpanderStack, IFExpanderStack
+from conv_upsamplers import LFExpanderStack, IFExpanderStack
 import tft
 import torch
 import torch.nn as nn
