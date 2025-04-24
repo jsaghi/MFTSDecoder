bc26597d00f85ec4a6a913524b55307061840ba1
Jeno <jeno.2010@yahoo.com>
2025-04-23 20:06:40 -0600

    changed the way load_with_model initializes the class again

--- source/mftft.py
+++ source/mftft.py
@@ -117,8 +117,8 @@ class LightningMFTFT(L.LightningModule):
     return optimizer
   
   @classmethod
-  def load_with_model(cls, checkpoint_path, model_class, *args, **kwargs):
-      model = model_class(*args, **kwargs)
+  def load_with_model(cls, checkpoint_path, model_class, dataset):
+      model = model_class(dataset)
       wrapper = cls(model)
       checkpoint = torch.load(checkpoint_path)
       wrapper.load_state_dict(checkpoint['state_dict'])
