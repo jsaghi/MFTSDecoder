e4ccf20835c2f79358dc47babe487e1d90b6afc6
Jeno Saghi <jeno.2010@yahoo.com>
2025-05-03 12:07:42 -0600

    fixed lstm input size

--- source/settings.py
+++ source/settings.py
@@ -23,7 +23,7 @@ STUDY_PATH = '../studies/'
 
 # Settings for the LSTM prediction model
 NUM_LSTM_LAYERS = 2
-LSTM_INPUT_SIZE = 14
+LSTM_INPUT_SIZE = 13
 LSTM_HIDDEN_SIZE = 256
 LSTM_DROPOUT = 0.5
 LSTM_LR = 1e-4
