41b13ca6db20d4b7432136d9db5442f854ba967a
Jeno <jeno.2010@yahoo.com>
2025-05-07 01:46:58 -0600

    more troubleshooting

--- source/tft.py
+++ source/tft.py
@@ -70,7 +70,9 @@ class LightningTFT(L.LightningModule):
     loss = loss_fn(y_hat, y)
 
     # Use median quantile for point estimate
-    y_median = y_hat[:, :, 1]
+    median_idx = self.tft_model.quantiles.index(0.5)
+    y_median = y_hat[..., median_idx]
+    #y_median = y_hat[:, :, 1]
 
     mae = self.logging_metrics['mae'](y_median, y)
     mse = self.logging_metrics['mse'](y_median, y)
