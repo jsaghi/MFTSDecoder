0a63f968ecb5284647ad3b266d1eae7c51065f58
Jeno <jeno.2010@yahoo.com>
2025-05-07 01:23:05 -0600

    bug fix

--- source/tft.py
+++ source/tft.py
@@ -65,8 +65,8 @@ class LightningTFT(L.LightningModule):
 
   def test_step(self, batch, batch_idx):
     x, y = batch
-    loss_fn = self.mftft_model.loss
-    y_hat = self.mftft_model(x)[0]
+    loss_fn = self.tft_model.loss
+    y_hat = self.tft_model(x)[0]
 
     loss = loss_fn(y_hat, y)
 
