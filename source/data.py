2a82b254d8e823d317343a96717a871d6ea958ba
Jeno <jeno.2010@yahoo.com>
2025-05-10 09:17:29 -0600

    testing third quarter of dataset

--- source/data.py
+++ source/data.py
@@ -387,8 +387,9 @@ def get_mfts():
   #data = raw_data.copy()
 
   # Code to use only a fraction of the full jena dataset
-  q1_index = int(len(raw_data) // 4)
-  data = raw_data.iloc[:q1_index, :]
+  q_index_start = int(len(raw_data) // 2)
+  q_index_end = int(len(raw_data) // 4 * 3)
+  data = raw_data.iloc[q_index_start:q_index_end, :]
 
   dataset = build_time_series(data)
 
